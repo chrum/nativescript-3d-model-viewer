96040e8d6ad2de21934d358829fa24ede2618038
chrystian <chrystianr@gmail.com>
2024-03-18 13:42:41 +0100

    feat(ios): initial ios code

--- packages/nativescript-3d-model-viewer/index.ios.ts
+++ packages/nativescript-3d-model-viewer/index.ios.ts
@@ -1,5 +1,35 @@
 import { ModelViewerCommon } from './common';
 
 export class ModelViewer extends ModelViewerCommon {
+    private _scene;
+    private _sceneView;
 
+    createNativeView() {
+        this._sceneView = SCNView.alloc()
+            .initWithFrame(CGRectMake(0, 0, 200, 200));
+
+        this._sceneView.allowsCameraControl = true;
+        this._sceneView.autoenablesDefaultLighting = true
+
+        return this._sceneView;
+    }
+    initNativeView() {
+        // optional
+    }
+    disposeNativeView() {
+        // this._sceneView.
+    }
+
+
+    public onLoaded(): void {
+        super.onLoaded();
+        this._scene = SCNScene.alloc().init();
+
+        const boxGeometry = SCNBox.boxWithWidthHeightLengthChamferRadius(10, 10, 10, 1)
+        const boxNode = SCNNode.nodeWithGeometry(boxGeometry);
+        this._scene.rootNode.addChildNode(boxNode);
+
+        const model = SCNScene.sceneNamed('Models.scnassets/chrum.dae');
+        this._sceneView.scene = model;
+    }
 }
--- packages/nativescript-3d-model-viewer/references.d.ts
+++ packages/nativescript-3d-model-viewer/references.d.ts
@@ -1,4 +1,7 @@
 /// <reference path="../../references.d.ts" />
+
+/// <reference path="../../node_modules/@nativescript/types-ios/lib/ios/objc-x86_64/objc!SceneKit.d.ts" />
+
 /// <reference path="../../types/sceneform.core.android.d.ts" />
 /// <reference path="../../types/sceneform.ux.android.d.ts" />
 /// <reference path="./typings/android.d.ts" />
